4ce2be74714989904ff609559f54fd9d05d77dc5
Martin Page <wookoouk@gmail.com>
2015-04-10 09:33:17 +0100

    tidy up

--- app.js
+++ app.js
@@ -19,32 +19,32 @@ var app = express();
 
 
 var genSecret = function () {
-    var secret = "", rand;
-    for (var i = 0; i < 36; i++) {
-        rand = Math.floor(Math.random() * 15);
-        if (rand < 10) {
-            // for 0-9
-            secret += String.fromCharCode(48 + rand);
-        } else {
-            // for a-f
-            secret += String.fromCharCode(97 + (rand - 10));
-        }
+  var secret = "", rand;
+  for (var i = 0; i < 36; i++) {
+    rand = Math.floor(Math.random() * 15);
+    if (rand < 10) {
+      // for 0-9
+      secret += String.fromCharCode(48 + rand);
+    } else {
+      // for a-f
+      secret += String.fromCharCode(97 + (rand - 10));
     }
-    return secret;
+  }
+  return secret;
 };
 
 app.use(session({
-    secret: genSecret(),
-    // cookie: {
-    // expires: false
-    // },
-    store: new MongoStore({
-        host: DB_HOST,
-        port: DB_PORT,
-        db: DB_NAME
-    }),
-    resave: true,
-    saveUninitialized: true
+  secret: genSecret(),
+  // cookie: {
+  // expires: false
+  // },
+  store: new MongoStore({
+    host: DB_HOST,
+    port: DB_PORT,
+    db: DB_NAME
+  }),
+  resave: true,
+  saveUninitialized: true
 }));
 
 app.use(passport.initialize());
@@ -52,31 +52,31 @@ app.use(passport.session());
 app.use(flash());
 
 app.use(function (req, res, next) {
-    var info = req.flash('info');
-    var error = req.flash('error');
-    var success = req.flash('success');
+  var info = req.flash('info');
+  var error = req.flash('error');
+  var success = req.flash('success');
 
-    if (info.length > 0) {
-        res.locals.info = info;
-    }
-    if (success.length > 0) {
-        res.locals.success = success;
-    }
-    if (error.length > 0) {
-        res.locals.error = error;
-    }
-    next(null, req, res);
+  if (info.length > 0) {
+    res.locals.info = info;
+  }
+  if (success.length > 0) {
+    res.locals.success = success;
+  }
+  if (error.length > 0) {
+    res.locals.error = error;
+  }
+  next(null, req, res);
 });
 
 //DB
 var db = mongoose.connection;
 var dbURI = DB_URI;
 db.on('error', function (error) {
-    console.log('mongoose error', error);
-    mongoose.disconnect();
+  console.log('mongoose error', error);
+  mongoose.disconnect();
 });
 db.on('disconnected', function () {
-    mongoose.connect(dbURI, {server: {auto_reconnect: true}});
+  mongoose.connect(dbURI, {server: {auto_reconnect: true}});
 });
 mongoose.connect(dbURI, {server: {auto_reconnect: true}});
 
@@ -91,13 +91,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(function (req, res, next) {
 
-    if (req.user != null) {
-        res.locals.signedInUser = {};
-        res.locals.signedInUser.username = req.user.username;
-    }
-    next(null, req, res);
+  if (req.user != null) {
+    res.locals.signedInUser = {};
+    res.locals.signedInUser.username = req.user.username;
+  }
+  next(null, req, res);
 });
-
 
 app.use('/', authRoute);
 app.use('/', indexRoute);
